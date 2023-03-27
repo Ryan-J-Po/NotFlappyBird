a5a0bfceb67da923a690e059a307c10516b3fdcc
Ryan <s228032@students.aie.edu>
2023-03-27 17:03:16 -0500

    Made Pipe Pair Template, basic NotFlappy mechanics

---
+++ Assets/Scripts/NotFlapBehaviour.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotFlapBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private float _jumpPower;
+
+    private Rigidbody _rigidBody;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            _rigidBody.AddForce(Vector3.up * _jumpPower, ForceMode.Impulse);
+        }
+    }
+}
